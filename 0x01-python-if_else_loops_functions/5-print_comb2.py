f387f4a1c6296881d6ef343d14371bc7d3c0a643
Kisange5 <kisangelorraine@gmail.com>
2023-06-06 17:26:07 -0700

    Write a function that checks for lowercase character.

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python3
-for number in range(0, 100):
-    if number < 99:
-        print("{:02d}".format(number), end=", ")
+for n in range(0, 100):
+    if n < 99:
+        print("{:02d}".format(n), end=", ")
     else:
-        print("{02d}".format(number)
+        print("{:2d}".format(n))
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0, 10):
+
+for i in range(10):
+    for j in range(i, 10):
         if i == 8 and j == 9:
-            print("{:d}{:d}".format(i, j)
-                    elif i < j and j != i:
-                    print("{:d}{:d}, ".format(i, j), end="")
+            print("{:d}{:d}".format(i, j))
+        elif i < j and i != j:
+            print("{:d}{:d}".format(i, j), end=", ")
