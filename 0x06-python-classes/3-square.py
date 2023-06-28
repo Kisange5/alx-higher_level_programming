bc929a03c6713c3ff23fa661996cf3d97ce89373
Kisange5 <kisangelorraine@gmail.com>
2023-06-28 15:05:08 -0700

    A function that Writes a class Square that defines a square by: (based on 2-square.py)

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""A module that contains class of type square"""
+
+
+class Square:
+    """class Square as size"""
+    def __init__(self, size=0):
+        """This initializes the size attribute
+        Args:
+            size (int): the size of the square
+        Raises:
+              TypeError: size must be an integer
+              ValueError: size must be >= 0
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif type(size) is int and size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """This initialize the area attribute
+        Return:
+              Area of a square
+        """
+        return self.__size ** 2
