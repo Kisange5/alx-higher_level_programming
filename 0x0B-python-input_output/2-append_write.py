2f2ae7145f629767106d5c20fe78724d6e66d612
Kisange5 <kisangelorraine@gmail.com>
2023-07-15 12:14:51 -0700

    Identation debug

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """module documentatation"""
+
+
 def append_write(filename="", text=""):
     """appends a string at the end of a textfile
     Args:
--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """module documentation"""
 
+
 import json
+
 
 def from_json_string(my_str):
     """creates an object from str
