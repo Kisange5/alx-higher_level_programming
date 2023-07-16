49d0b8845104ccb7716a8ea4f825acf2bd966979
Kisange5 <kisangelorraine@gmail.com>
2023-07-16 03:56:56 -0700

    A  class Student that defines a student by: (based on 9-student.py)

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""module documentation"""
+
+
+class Student:
+    """A class that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """class initializer"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrievs dict rep of the class"""
+        rep = self.__dict__.copy()
+        if type(attrs) is list:
+            n_rep = {}
+            for i in attrs:
+                if type(i) is not str:
+                    return rep
+                if i in rep:
+                    n_rep[i] = rep[i]
+                    return n_rep
+                return rep
