34722a140f96655e99aba8957ced5b3d76ccfde6
Kisange5 <kisangelorraine@gmail.com>
2023-07-17 14:35:08 -0700

    A  first class Base

---
+++ 0x0C-python-almost_a_circle/README.md
@@ -0,0 +1,10 @@
+0x0C. Python - Almost a circle
+Tasks
+0. If it's not tested it doesn't work
+mandatory
+All your files, classes and methods must be unit tested and be PEP 8 validated.
+Repo:
+
+GitHub repository: alx-higher_level_programming
+Directory: 0x0C-python-almost_a_circle
+File: tests/
---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""module documentation"""
+
+
+import json
+
+
+class Base:
+    """class documentation"""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """module init
+        Args:
+            id (int): the item id
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "{}"
+        else:
+            return json.dumps(list_dictionaries)
